04533b9d24891b95b01fdb460ed2b100fb8e4bd4
SLPowerCoder <sunlei_1030@126.com>
2016-08-31 14:42:48 +0800

    Initial Commit

---
+++ AnimationSet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationSet
+//
+//  Created by sunlei on 16/8/31.
+//  Copyright © 2016年 sunlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
